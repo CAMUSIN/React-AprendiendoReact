bac34552f43ba1688f289777065f7c0aaf8b8c3e
CAMUSIN <fergg8704@gmail.com>
2023-06-16 00:09:19 -0600

    Using childrens, arrays and key

--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react"
 
-export function TwitterFollowCard({ formatUserName, userName, name, initialIsFollowing}){
+export function TwitterFollowCard({ children, formatUserName, userName, initialIsFollowing}){
     const [ following, setFollowing ] = useState(initialIsFollowing)
-    const textIsFollowing = following ? 'Siguiendo' : 'Seguir'
+    const textIsFollowing = following ? 'Following' : 'Follow'
     const buttonClassName = following ? 'tw-followCard-button is-following' : 'tw-followCard-button'
 
     function handleClick(){
@@ -12,15 +12,18 @@ export function TwitterFollowCard({ formatUserName, userName, name, initialIsFol
     return (
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
-                <img className='tw-followCard-avatar' alt="El Avatar de midudev" 
+                <img className='tw-followCard-avatar' alt="Avatar" 
                     src={`https://unavatar.io/github/${ userName }`}/>
                 <div className='tw-followCard-info'>
-                    <strong>{ name }</strong>
+                    <strong>{ children }</strong>
                     <span  className='tw--infoUserName'>{ formatUserName(userName) }</span>
                 </div>
             </header>
             <aside>
-                <button onClick={handleClick} className={buttonClassName}>{ textIsFollowing }</button>
+                <button onClick={handleClick} className={buttonClassName}>
+                    <span className="tw-followCard-isfollow">{ textIsFollowing }</span>
+                    <span className="tw-followCard-unfollow">Unfollow</span>
+                </button>
             </aside>
         </article>
     )
--- projects/00-hola-mundo/src/assets/App.jsx
+++ projects/00-hola-mundo/src/assets/App.jsx
@@ -4,11 +4,40 @@ import './App.css'
 export function App(){
     const formatUserName = (userName) => `@${userName}`
 
+const users = [
+    {
+        userName:"midudev",
+        name:"Miguel Angel Duran",
+        isFollowing: true
+    },
+    {
+        userName:"camusin",
+        name:"Fernando Garcia",
+        isFollowing: false
+    },
+    {
+        userName:"pheralb",
+        name:"Pablo Hernandez"
+    },
+    {
+        userName:"ObuchiYuki",
+        name:"Obuchi Yuki"
+    },
+]
+
     return (
         <div className='app-users'>
-            <TwitterFollowCard formatUserName={formatUserName} userName="midudev" name="Miguel Angel Duran" initialIsFollowing={true}/>
-            <TwitterFollowCard formatUserName={formatUserName} userName="pheralb" name="Pablo Hernandez"/>
-            <TwitterFollowCard formatUserName={formatUserName} userName="camusin" name="Fernando Garcia"/>
+            {
+                users.map(({ userName, name, isFollowing}) => (
+                    <TwitterFollowCard key={userName} 
+                    formatUserName={formatUserName} 
+                    userName={userName} 
+                    initialIsFollowing={isFollowing}
+                    >
+                        {name}
+                    </TwitterFollowCard>
+                ))
+            }
         </div>
     );
 }
